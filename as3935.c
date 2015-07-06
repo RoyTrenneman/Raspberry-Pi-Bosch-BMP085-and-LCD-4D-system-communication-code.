6bb6474e5acc7f824b252cc14035135bac9f72a6
Ross <traxxdtc@gmail.com>
2015-07-06 08:10:03 +0000

    Add library and code for interacting with the Franklin Lightning Sensor.

---
+++ as3935.c
@@ -0,0 +1,125 @@
+/*
+Raspberry Pi AS3935 I2C communication code.
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/ioctl.h>
+#include "smbus.h" 
+#define AS3935_I2C_ADDRESS 0x03
+
+unsigned int reg7 ;
+unsigned int reg8 ;
+unsigned int reg3 ;
+unsigned int reg0 ;
+unsigned int reg1 ;
+
+int AS3935_i2c_Begin()
+{
+int as;
+char *fileName = "/dev/i2c-1";
+   
+   // Open port for reading and writing
+ if ((as = open(fileName, O_RDWR)) < 0 )
+    exit(1);
+  
+   // Set the port options and set the address of the device
+ if (ioctl(as, I2C_SLAVE, AS3935_I2C_ADDRESS) < 0) {               
+    close(as);
+    exit(1);
+ }
+return as;
+}
+
+
+// Read block from the AS3935 
+void AS3935_i2c_Read_Int(int as, __u8 address, __u8 length, __u8 *values)
+{
+ if(i2c_smbus_read_i2c_block_data(as, address,length,values)<0) {
+      close(as);
+      exit(1);
+ }
+}
+//Write a byte to the AS3935
+void AS3935_i2c_Write_Byte(int as, __u8 address, __u8 value)
+{
+ if (i2c_smbus_write_byte_data(as, address, value) < 0) {
+   //  printf ("cannot write bytes for as3935:") ;
+     close(as);
+     exit(1);
+ }
+}
+
+int read_data(int i)
+{
+__u8 values[8];
+int as = AS3935_i2c_Begin();
+ AS3935_i2c_Read_Int(as, 0x00, 9, values);
+ close(as);
+return values[i];
+}
+
+void AS3935_Calibration()
+{
+int values;
+//CALIB_RCO: 
+int as = AS3935_i2c_Begin();
+//Set all registers in default mode
+//write 0x96 into Register 0x3C
+//	AS3935_i2c_Write_Byte(as,0x3C,0x96);
+ usleep(3000);
+//Calibrate Internal Capacitor to max (120pF)
+ values = read_data(8);
+//reg8 = AS3935_i2c_Read_Int(as,0x08);
+ usleep(3000);
+ AS3935_i2c_Write_Byte(as,0x08, (values & 0xF0) | 0x0f) ;
+ usleep(3000);
+//Recalibrating the automatically the RC ocsillators
+ AS3935_i2c_Write_Byte(as,0x3D,0x96);
+ usleep(3000);
+//Read Register 0x08; 
+ values = read_data(8);
+//reg8 = AS3935_i2c_Read_Int(as,0x08);
+ usleep(3000);
+//write 0x01 into Register 0x08[5]; 
+ AS3935_i2c_Write_Byte(as,0x08, (values | 0x20)) ;
+ usleep(3000);
+//Read Register 0x08; 
+ values = read_data(8);
+//reg8 = AS3935_i2c_Read_Int(as,0x08);
+ usleep(3000);
+//write 0x00 into Register 0x08[5]; 
+ AS3935_i2c_Write_Byte(as,0x08, (values & 0xdf)) ;
+ usleep(2000);
+//Set noise floor to 0x00 into register 0x01[6:4]
+ close(as);
+}
+
+unsigned int AS3935_returnDistance()
+{
+unsigned int distance;
+int values;
+//Read Register 0x07; 
+ values = read_data(7);
+//Return Bits[5:0]
+ distance = values & 0x3f;
+return distance;
+}
+
+int AS3935_returnInterrupt()
+{
+int interrupt;
+int values;
+//Read Register 0x03; 
+ values = read_data(3);
+//Return Bits[5:0]
+ interrupt = values & 0x0f;
+return interrupt;
+}
+
---
+++ as3935.h
@@ -0,0 +1,8 @@
+#ifndef LIB_AS_H
+#define LIB_AS_H
+
+void AS3935_Calibration();
+int AS3935_returnDistance();
+int AS3935_returnInterrupt();
+
+#endif
